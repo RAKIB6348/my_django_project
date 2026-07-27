7471dcf23cdd0a11ae68fd02ad248623c9f6586c
RAKIB6348 <shahidulhuda0@gmail.com>
2026-07-27 17:46:54 +0600

    Add list_display and fieldsets to AcademicYear and Section admin

--- academic/admin.py
+++ academic/admin.py
@@ -2,5 +2,29 @@ from django.contrib import admin
 from .models import AcademicYear, Section
 
 
-admin.site.register(AcademicYear)
-admin.site.register(Section)
+@admin.register(AcademicYear)
+class AcademicYearAdmin(admin.ModelAdmin):
+    list_display = ('year', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('year',)
+    ordering = ('-year',)
+
+    fieldsets = (
+        ('Academic Year Info', {
+            'fields': ('year', 'is_active')
+        }),
+    )
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'academic_year')
+    list_filter = ('academic_year',)
+    search_fields = ('name',)
+    ordering = ('academic_year', 'name')
+
+    fieldsets = (
+        ('Section Info', {
+            'fields': ('name', 'academic_year')
+        }),
+    )
