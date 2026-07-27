9a6fa81d32da9db6b826d7c862e1ea09e1c79204
RAKIB6348 <shahidulhuda0@gmail.com>
2026-07-27 18:35:19 +0600

    create students app urls file and set url file inside project urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('auth_user.urls')),
+    path('students/', include('students.urls')),
 ]
 
 if settings.DEBUG:
